6f86a81a514a46dd39c0e4c5f21dab9269fa0640
vasylky <vasylkyqvky@gmail.com>
2025-10-02 10:25:02 +0300

    update port config

--- app.py
+++ app.py
@@ -532,4 +532,4 @@ def get_reviews(flight_id):
 # =============================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
